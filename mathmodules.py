c6718fe3e393baeb5d65d0ed522aa0fe3ce5d8bd
darveshkumar1610 <darveshkumar1610@gmail.com>
2021-12-26 18:05:35 +0400

    Create mathmodules.py

---
+++ mathmodules.py
@@ -0,0 +1,25 @@
+#### math.ceil() ####
+
+import math
+x=12.8
+print(math.ceil(x))         # Output=13
+
+#### math.floor() ####
+x=10.5
+print(math.floor(x))         # Output=10
+
+#### math.fabs() ####
+x=-10
+print(math.fabs(x))         # Output=10
+
+#### math.factorial() ####
+x=5
+print(math.factorial(x))    # Output=120
+
+#### math.fsum() ####
+l=[10,20,30,40]
+print(math.fsum(l))        # Output=100.0
+
+#### math.sqrt() ####
+x=25
+print(math.sqrt(x))        # Output=5.0
