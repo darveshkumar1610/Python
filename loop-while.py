4f07e3313d78d2fe38c50bc15733a3b724ca9ce7
darveshkumar1610 <darveshkumar1610@gmail.com>
2021-12-26 16:00:49 +0400

    Create loop-while.py

---
+++ loop-while.py
@@ -0,0 +1,19 @@
+############## Increment #############
+#start
+#condition
+#increment/decrement
+
+i=1
+while 1<=10:
+  print(i, "Welcome to the world!!!")
+  i=i+1
+
+print(i)
+
+############## Decrement #############
+a = 10
+while a>=1:
+  print(a, "hello")
+  a=a-1
+  
+print(a)
